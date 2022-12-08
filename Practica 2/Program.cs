72f7b22ef917f0a812fdb515fc43472878bab0c4
alejandro ornelas montoya <hussong1234567@live.com>
2022-12-07 21:26:56 -0800

    Agregar archivos de proyecto.

---
+++ Practica 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 23;
+            string Nombre = "Alejandro";
+            string Pais = "Mexico";
+
+            Console.WriteLine("Mi nombre es: {0} tengo: {1} y soy de: {2}", Nombre, edad, Pais);
+            Console.ReadKey();
+        }
+
+    }
+}
+
+
+
